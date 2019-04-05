05a562a1347ff49c3b63ba8bf385bb4ad5f4e1e2
BraydenMiller <48822485+BraydenMiller@users.noreply.github.com>
2019-04-05 15:15:16 +0800

    Added a Readme file

---
+++ StarSkirmish/Assets/Scripts/_ReadMe.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _ReadMe : MonoBehaviour
+{
+/* 
+ README FILE FOR STAR SKIRMISH
+     
+     
+     
+     */
+}
