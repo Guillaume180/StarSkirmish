22a9cdb75ddd70dc33622a511de1e75c106967c6
Guillaume180 <43747099+Guillaume180@users.noreply.github.com>
2019-04-05 15:36:59 +0800

    Changes
    made a lot of changes

---
+++ StarSkirmish/Assets/Scripts/ChangeScenes.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScenes : MonoBehaviour
+{
+    //Simple script to make the button select certain levels.
+    public void ChangeTheScene(int SceneNumber)
+    {
+        SceneManager.LoadScene(SceneNumber);
+    }
+}
