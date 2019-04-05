ddd62a711970a3bb200b3f0ec3176bd8e2f791ec
MacVP <48820988+MacVP@users.noreply.github.com>
2019-04-05 16:57:49 +0800

    Added Star Bucks Image and Store Edit

--- StarSkirmish/Assets/Scripts/_ReadMe.cs
+++ StarSkirmish/Assets/Scripts/_ReadMe.cs
@@ -10,6 +10,7 @@ public class _ReadMe : MonoBehaviour
     Downloaded Pixel Inventory by David Douglas Retrieved from: https://www.pinterest.com.au/pin/795448352904373538/
     ^^^^^^^^^^^^^^Link to UI for menus^^^^^^^^^^^^^^^^^^
 
+    Download “STAR MONEY” (LOGO DESIGN) by Lady K from: http://memphiswebmarketing.com/dreamteamgraphics/?p=3134
 
          */
 }
