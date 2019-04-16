d59d9b4a6153f030f1d487dc7cc50ea1502a7081
Guillaume180 <43747099+Guillaume180@users.noreply.github.com>
2019-04-16 14:56:12 +0800

    Changes
    Added the time script

---
+++ StarSkirmish/Assets/Scripts/TimerScript.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TimerScript : MonoBehaviour
+{
+    //This is how we will contril the time for our game.
+    public enum TimeState {Tutorial, Unpaused, Countdown, Paused, Gameover, Complete};
+    public TimeState state = TimeState.Countdown;
+
+    //If we are going to put a timer in our game.
+    public Text timeText;
+
+    public GameObject gameIsOverScreen;
+    public GameObject gameIsCompleteScreen;
+    public GameObject endOfLevelScreen;
+    public GameObject gameIsPauseScreen;
+    public GameObject tutorialScreen;
+
+    public int time;
+
+    private void Update()
+    {
+        switch(state)
+        {
+            case (TimeState.Tutorial):
+                TutorialScreen();
+                break;
+            case (TimeState.Unpaused):
+                UnpauseTheGame();
+                break;
+            case (TimeState.Countdown):
+                CountdownTime();
+                break;
+            case (TimeState.Paused):
+                PauseTheGame();
+                break;
+            case (TimeState.Gameover):
+                GameisOver();
+                break;
+            case (TimeState.Complete):
+                GameIsComplete();
+                break;
+        }
+
+        if(time <= 0)
+        {
+            StopCoroutine("LoseTime");
+            state = TimeState.Gameover;
+        }
+    }
+
+    void CountdownTime()
+    {
+        StartCoroutine("LoseTime");
+    }
+
+    IEnumerator LoseTime()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(1);
+            time--;
+        }
+    }
+
+    public void  TutorialScreen()
+    {
+        Time.timeScale = 0;
+        tutorialScreen.SetActive(true);
+    }
+
+    public void UnpauseTheGame()
+    {
+        Time.timeScale = 1;
+        gameIsPauseScreen.SetActive(false);
+        tutorialScreen.SetActive(false);
+        state = TimeState.Countdown;
+    }
+
+    public void PauseTheGame()
+    {
+        Time.timeScale = 0;
+        gameIsPauseScreen.SetActive(true);
+    }
+
+    public void GameisOver()
+    {
+        Time.timeScale = 0;
+        gameIsOverScreen.SetActive(true);
+    }
+
+    public void GameIsComplete()
+    {
+        Time.timeScale = 0;
+        gameIsCompleteScreen.SetActive(true);
+    }
+
+
+}
