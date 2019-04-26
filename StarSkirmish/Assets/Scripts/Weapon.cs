e980ac7b16a2177a6116bebcacc8b4186d1a6b70
Guillaume180 <43747099+Guillaume180@users.noreply.github.com>
2019-04-26 17:35:20 +0800

    dsh

--- StarSkirmish/Assets/Scripts/Weapon.cs
+++ StarSkirmish/Assets/Scripts/Weapon.cs
@@ -7,6 +7,8 @@ public class Weapon : MonoBehaviour
     public Transform firepoint;
     public GameObject bulletPrefab;
 
+    public CharacterController weaponDirection;
+
     // Update is called once per frame
     void Update()
     {
@@ -20,13 +22,13 @@ public class Weapon : MonoBehaviour
     {
         GameObject bullet = Instantiate(bulletPrefab, firepoint.position, Quaternion.identity) as GameObject;
 
-        if (GetComponent<CharacterController>().facingRight)
+        if (weaponDirection.facingRight)
         {
-            bullet.GetComponent<Rigidbody2D>().AddForce(Vector2.right * 50, ForceMode2D.Impulse);
+            bullet.GetComponent<Rigidbody2D>().AddForce(Vector2.right * 10, ForceMode2D.Impulse);
         }
         else
         {
-            bullet.GetComponent<Rigidbody2D>().AddForce(Vector2.left * 50, ForceMode2D.Impulse);
+            bullet.GetComponent<Rigidbody2D>().AddForce(Vector2.left * 10, ForceMode2D.Impulse);
         }
     }
 }
