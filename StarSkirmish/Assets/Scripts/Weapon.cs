fb17cb09a8593017793e0f0adb801b881eba77fb
BraydenMiller <48822485+BraydenMiller@users.noreply.github.com>
2019-04-26 17:25:10 +0800

    cha

--- StarSkirmish/Assets/Scripts/Weapon.cs
+++ StarSkirmish/Assets/Scripts/Weapon.cs
@@ -22,11 +22,11 @@ public class Weapon : MonoBehaviour
 
         if (GetComponent<CharacterController>().facingRight)
         {
-            bullet.GetComponent<Rigidbody2D>().AddForce(Vector2.right * 10, ForceMode2D.Impulse);
+            bullet.GetComponent<Rigidbody2D>().AddForce(Vector2.right * 50, ForceMode2D.Impulse);
         }
         else
         {
-            bullet.GetComponent<Rigidbody2D>().AddForce(Vector2.left * 10, ForceMode2D.Impulse);
+            bullet.GetComponent<Rigidbody2D>().AddForce(Vector2.left * 50, ForceMode2D.Impulse);
         }
     }
 }
